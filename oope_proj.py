7c9939406618434847ef2cb7d0ba393612826fa2
Nabarun <nabarundutta307@gmail.com>
2025-01-21 22:35:17 +0530

    signup and signin completed

--- oope_proj.py
+++ oope_proj.py
@@ -10,16 +10,38 @@ class chatbook:
                            press 2 to sign in
                            press 3 to write a post
                            press 4 to message a friend
-                           press 5 to exit""")
+                           press 5 to exit
+                           \n""")
         if user_input=='1' :
-            pass
+            self.signup()
         elif user_input=='2':
-            pass
+            self.signin()
         elif user_input=='3':
             pass
         elif user_input=='4':
             pass
         else:
             exit()       
-            
+    
+    def signup(self):
+        username = input("Enter your email adress : ")
+        password = input("Enter your password : ")
+        self.username = username
+        self.password = password
+        print("You have successfully signed up")
+        self.menu()
+        
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("you have to sign up first \n")
+        else:
+            uname = input("Enter your username : ")
+            pwd = input("Enter your password : ")
+            if self.username==uname and self.password==pwd:
+                print("you have successfully signed \n")
+                self.loggedin() = True
+            else:
+                print("you have wrong input")
+        self.menu()
+    
 obj = chatbook()
